784dda741c4ab69988674a743ba392406d6dce7a
ddung203 <duongvandung2k3@gmail.com>
2024-04-05 21:23:04 +0700

    feat(no test): create user api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,15 @@ import express, { Router } from "express";
 import accessRouter from "./access/index";
 import transactionRouter from "./transaction/index";
 import commentRouter from "./comment/index";
+import bookRouter from "./book/index";
+import userRouter from "./user/index";
 
 const router: Router = express.Router();
 
 router.use("/v1/api", accessRouter);
 router.use("/v1/api", transactionRouter);
 router.use("/v1/api", commentRouter);
+router.use("/v1/api", bookRouter);
+router.use("/v1/api", userRouter);
 
 export default router;
---
+++ src/routes/user/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import asyncHandle from "../../helpers/asyncHandle.ts";
+import UserController from "./../../controllers/user.controller";
+
+const router = express.Router();
+
+router.get("/user/list", asyncHandle(UserController.getAllUser));
+router.get("/user/getByUsername", asyncHandle(UserController.getByUsername));
+router.get("/user/getById", asyncHandle(UserController.getById));
+
+router.get(
+  "/user/deleteByUsername",
+  asyncHandle(UserController.deleteUserByUsername)
+);
+router.get("/user/deleteById", asyncHandle(UserController.deleteUserById));
+
+router.put("/user/update", asyncHandle(UserController.update));
+
+export default router;
