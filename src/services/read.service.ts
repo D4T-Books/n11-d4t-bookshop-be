c4cb0e917a6dddd02870f8b75051f9df87d1bba3
ddung203 <duongvandung2k3@gmail.com>
2024-05-29 01:38:38 +0700

    bug: queryTrack

--- src/services/read.service.ts
+++ src/services/read.service.ts
@@ -26,26 +26,63 @@ class ReadService {
       title_for_search: string;
     }
   ): Promise<any> => {
-    const query1 =
-      "SELECT * FROM book_tracking WHERE Username = ? AND title_for_search = ?";
+    // Kiểm tra giá sách
+    const queryPrice = `
+      SELECT Price 
+      FROM books 
+      WHERE title_for_search = ?`;
+    const [book] = await queryToDatabase(queryPrice, [title_for_search]);
 
-    let [track] = await queryToDatabase(query1, [Username, title_for_search]);
-
-    if (track.length === 0) {
-      const query =
-        "INSERT INTO book_tracking (Username, title_for_search) VALUES (?, ?)";
-
-      await queryToDatabase(query, [Username, title_for_search]);
+    if (!book || book.length === 0) {
+      throw new BadRequestError("Sách không tồn tại!");
     }
 
-    // !?
+    // Kiểm tra trạng thái mua sách
+    const queryTrack = `
+      SELECT * 
+      FROM book_tracking 
+      WHERE Username = ? 
+      AND title_for_search = ? AND isBought = 0`;
+    let [track] = await queryToDatabase(queryTrack, [
+      Username,
+      title_for_search,
+    ]);
 
-    [track] = await queryToDatabase(query1, [Username, title_for_search]);
+    console.log("track.length :>> ", track.length);
 
-    return {
-      track: track[0],
-    };
+    if (track.length === 0 && book[0].Price == 0) {
+      // Chèn thông tin theo dõi sách nếu chưa tồn tại và sách miễn phí
+      const queryInsert = `
+        INSERT INTO book_tracking (Username, title_for_search, isBought) 
+        VALUES (?, ?, 1)`;
+      await queryToDatabase(queryInsert, [Username, title_for_search]);
+
+      // Truy vấn lại để lấy thông tin theo dõi vừa được thêm vào
+      const queryTrackAfterInsert = `
+        SELECT * 
+        FROM book_tracking 
+        WHERE Username = ? 
+        AND title_for_search = ? AND isBought = 1`;
+
+      [track] = await queryToDatabase(queryTrackAfterInsert, [
+        Username,
+        title_for_search,
+      ]);
+
+      if (!track || track.length === 0) {
+        throw new Error(
+          "Đã xảy ra lỗi trong quá trình thêm thông tin theo dõi sách."
+        );
+      }
+
+      return {
+        track: track[0],
+      };
+    } else {
+      throw new BadRequestError("Bạn chưa mở khóa sách này!");
+    }
   };
+
   static trackingFavoriteBook = async (
     { Username }: { Username: string },
     {
